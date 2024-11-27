0cc54294b944bcacc710f10a6b45e83d43357daa
Leimila <leimilamitei2016@gmail.com>
2024-11-27 23:05:11 +0300

    eact-hooks-import-export-lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import NavBar from "./NavBar"; // Import Navbar component
+import Home from "./Home";     // Import Home component
+import About from "./About";   // Import About component
 
 function App() {
   return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
+    <div className="App">
+      <NavBar />  
+      <Home />    
+      <About />   
     </div>
   );
 }
